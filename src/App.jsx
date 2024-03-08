4ddc16c44f337f354eee9440b4bd10c1e04a2564
Adarsh <adarshravi011@gmail.com>
2024-03-08 09:47:59 +0530

    env added and transition changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import Weather from './components/Weather'
 
 function App() {
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import  axios  from 'axios'
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import clear from '../assets/clear.jpg'
 import cloudy from '../assets/cloudy.jpg'
@@ -10,86 +10,95 @@ import thunderStorm from '../assets/thunderStorm.jpg'
 
 const Weather = () => {
 
-    const [city,setCity] = useState('delhi')
-    const [weather,setWeather] = useState({})
-    const [image,setImage] = useState('')
+    const [city, setCity] = useState('delhi')
+    const [weather, setWeather] = useState({})
+    const [image, setImage] = useState('')
 
-    useEffect(()=>{
+    const operWeatherApi = import.meta.env.VITE_OPENWEATHERAPI;
+
+    useEffect(() => {
         // setWeather({...weather,date:formatDate(new Date())})
         setWeatherHandle()
-    },[])
 
-    const getWeather = async(e)=>{
+    }, [])
+
+    const getWeather = async (e) => {
         e.preventDefault()
+        setWeatherHandle()
+    }
+
+    const setWeatherHandle = async () => {
         try {
-           setWeatherHandle()
+            const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${operWeatherApi}&units=metric`)
+            // console.log(res)
+            setCity('')
+            let code = res.data.weather[0].id
+            if (code > 800) {
+                setImage(cloudy)
+            }
+            else if (code == 800) {
+                setImage(clear)
+            }
+            else if (code < 800 && code > 700) {
+                setImage(mist)
+            }
+            else if (code >= 600 && code < 700) {
+                setImage(snow)
+            }
+            else if (code >= 500 && code < 600) {
+                setImage(rain)
+            }
+            else if (code >= 300 && code < 500) {
+                setImage(drizzle)
+            }
+            else if (code >= 200 && code < 300) {
+                setImage(thunderStorm)
+            }
+
+
+            setWeather({
+                ...weather,
+                temperature: res.data.main.temp,
+                maxTemp: res.data.main.temp_max,
+                minTemp: res.data.main.temp_min,
+                humidity: res.data.main.humidity,
+                wind: res.data.wind.speed,
+                visibility: res.data.visibility / 1000,
+                location: res.data.name,
+                weather: res.data.weather[0].main,
+                icon: res.data.weather[0].icon,
+                date: formatDate(new Date())
+            })
         } catch (error) {
             console.log(error)
         }
-    }
-
-    const setWeatherHandle = async()=>{
-        const res =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=18f8ed9769c1b8ef31af7a3bf6e7ebc8&units=metric`)
-        let code = res.data.weather[0].id
-        if(code > 800){
-            setImage(cloudy)
-        }
-        else if(code == 800){
-            setImage(clear)
-        }
-        else if(code < 800 && code > 700){
-            setImage(mist)
-        }
-        else if(code >= 600 && code < 700){
-            setImage(snow)
-        }
-        else if(code >=500 && code < 600){
-            setImage(rain)
-        }
-        else if(code >= 300 && code < 500){
-            setImage(drizzle)
-        }
-        else if(code >=200 && code <300){
-            setImage(thunderStorm)
-        }
-
-
-        setWeather({...weather,
-            temperature:res.data.main.temp,
-            maxTemp : res.data.main.temp_max,
-            minTemp : res.data.main.temp_min,
-            humidity : res.data.main.humidity,
-            wind: res.data.wind.speed,
-            visibility : res.data.visibility / 1000,
-            location : res.data.name,
-            weather : res.data.weather[0].main,
-            icon : res.data.weather[0].icon,
-            date:formatDate(new Date())
-        })
     }
 
 
     function formatDate(date) {
         const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        
+
         const dayOfWeek = daysOfWeek[date.getDay()];
         const day = date.getDate();
         const month = months[date.getMonth()];
         const year = date.getFullYear();
-        
+
         return `${dayOfWeek} ${day} ${month} ${year}`;
-      }
+    }
 
 
 
     return (
-        <div className="min-h-screen flex items-center justify-center" style={{backgroundImage: `url(${image})`,backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'}}>
+        <div className="min-h-screen flex items-center justify-center" style={{
+            backgroundImage: `url(${image})`, backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            transition: 'background-image 1s linear'
+        }}>
             <div className="flex flex-col bg-white bg-opacity-10 backdrop-blur-sm rounded p-4 w-full max-w-xs shadow-lg">
-                <form className='mb-2 rounded border border-gray-400' onSubmit={(e)=>getWeather(e)}>
-                    <input type="text" className='w-3/4 bg-transparent  outline-none p-2' value={city} onChange={(e)=>setCity(e.target.value)}/>
+                <form className='mb-2 rounded border border-gray-400' onSubmit={(e) => getWeather(e)}>
+                    <input type="text" className='w-3/4 bg-transparent  outline-none p-2' value={city} onChange={(e) => setCity(e.target.value)} />
                     <button className='w-1/4  p-2 text-white'>Search</button>
                 </form>
                 <div className="font-bold text-3xl">{weather.location}</div>
